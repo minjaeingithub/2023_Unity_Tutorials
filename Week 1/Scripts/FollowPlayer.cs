9c41889e731565849b02c67d03a91a29c21fc532
Minjae Jo <ashley05190519@gmail.com>
2023-01-06 16:27:11 +0900

    Uploaded Week 1 script files

---
+++ Week 1/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 8, -15);
+
+    void LateUpdate()
+    {
+        // 1. Camera will follow player. Recap why.
+        // Answer: Camera's position is assigned as player's position in scene.
+        // 2. By adding new 3 coordinates, we could change offset for every frame updates.
+        // 3. What happens when using LateUpdate()?
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Week 1/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 15.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    void Update()
+    {
+      horizontalInput = Input.GetAxis("Horizontal");
+      forwardInput = Input.GetAxis("Vertical");
+      // Move forward
+      transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+      // Turn 
+      transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
